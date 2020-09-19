455b345ea3617a3c00e6a6600d0b033d438d39a2
Aiman Irfan <aimanirfan1017@gmail.com>
2020-09-19 15:03:37 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// const express = require('express');
+// const bodyParser = require('body-parser')
+// const app = express();
+
+// app.use(bodyParser.urlencoded({extended: true}));
+// app.use(bodyParser.json());
+
+// const port = process.env.PORT || 8000;
+// const router = express.Router();
+
+// router.get('/HomePage', (req, res) => {
+//     res.sendFile(__dirname + '/index.html')
+// })
+
+// router.get('/pickDate', (req, res) => {
+//     res.sendFile(__dirname + '/pickdate.html')
+// });
+
+// router.get('/userinformation', (req, res) => {
+//     res.sendFile(__dirname + '/userInfromation.html')
+// });
+
+// app.use('/api', router);
+// app.listen(port);
+// console.log('Magic happens on part ' + port);
